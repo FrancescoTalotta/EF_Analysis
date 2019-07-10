09ef5c049731d2b3887f7158bcd7411877601630
Francesco <francesco@macbook-2.local>
2019-07-10 13:43:00 +0100

    norm

--- main.f90
+++ main.f90
@@ -1,15 +1,14 @@
-program EF_analysis 
+program EF_analysis
 
  implicit none
- 
+
  complex(16), allocatable :: chi(:,:), chi_dt(:,:), chi_dR(:,:,:)
- real(8),     allocatable :: V(:,:,:), w(:), tdPES_BO(:), grid_value(:,:)
+ real(8),     allocatable :: V(:,:,:), w(:), tdPES_BO(:), grid_value(:,:), G(:,:)
  integer                  :: n_dof, n_grid, nstates, time_steps
- real(8),     allocatable :: G(:,:)
 
  integer                  :: i, j, nlines, io, time
-
  character(100)           :: pot_file, chi_file, nullo
+ complex(16)              :: norm
 
  pot_file='EF_parameter_gV_1024'
  chi_file='EF_parameter_dpsi_1024'
@@ -22,14 +21,14 @@ program EF_analysis
 
    allocate(V(nstates,nstates,n_grid), w(n_grid), grid_value(n_grid,n_dof), G(n_dof,n_dof))
 
-   read(100,*) 
-   read(100,*) 
-   do i=1,n_dof 
-    read(100,*) j,G(:,i) 
+   read(100,*)
+   read(100,*)
+   do i=1,n_dof
+    read(100,*) j,G(:,i)
    enddo
 
-   read(100,*) 
-   read(100,*) 
+   read(100,*)
+   read(100,*)
 
    do i=1,n_grid
     read(100,*) nullo, j, w(i), grid_value(i,:), V(:,:,i)
@@ -38,13 +37,13 @@ program EF_analysis
  close(100)
 
  open(file=chi_file,unit=100,status='old',action='read')
-  
-   nlines = 0 
+
+   nlines = 0
    do
     read(100,*,iostat=io)
     if(io/=0) exit
     nlines = nlines + 1
-   enddo 
+   enddo
 
  close(100)
 
@@ -53,16 +52,21 @@ program EF_analysis
  allocate(chi(n_grid,nstates),chi_dt(n_grid,nstates),chi_dR(n_grid,n_dof,nstates))
 
  open(file=chi_file,unit=100,status='old',action='read')
-  
- read(100,*) 
+
+ read(100,*)
 
   do time=1,time_steps
    do i=1,n_grid
     read(100,*) nullo, nullo, grid_value(i,:), chi(i,:), chi_dt(i,:), chi_dR(i,:,:)
    enddo
    read(100,*)
+   do j=1,nstates
+    norm=dot_product(chi(:,j),w(:)*chi(:,j))
+    write(*,*) norm
+   enddo
   enddo
 
  close(100)
- 
-end program EF_analysis 
+
+
+end program EF_analysis
